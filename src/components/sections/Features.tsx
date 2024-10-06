254b616fd53d202f49f928a5575ebe0d3a274f21
alanegd <alangalavisd@gmail.com>
2024-10-06 10:21:05 -0500

    chore: Update landing page images

--- src/components/sections/Features.tsx
+++ src/components/sections/Features.tsx
@@ -6,7 +6,10 @@ import Image from "next/image";
 
 const Features = () => {
   return (
-    <div id="features" className="relative flex flex-col xl:flex-row justify-evenly items-center w-full py-[12%] xl:py-0 xl:h-screen xl:gap-x-20 bg-primary-white text-primary-black">
+    <div
+      id="features"
+      className="relative flex flex-col xl:flex-row justify-evenly items-center w-full py-[12%] xl:py-0 xl:h-screen xl:gap-x-20 bg-primary-white text-primary-black"
+    >
       <div className="flex flex-col py-10 justify-center xl:justify-start items-center xl:items-start h-full xl:h-auto max-w-[32.8rem]">
         <h4 className="mb-5">What We Offer</h4>
         <Header className="mb-16" title="Features and Benefits" />
@@ -15,7 +18,6 @@ const Features = () => {
             icon="/icons/humidity.svg"
             title="Real-Time Insights"
             description={`Provide accurate, real-time weather information using <span class="font-bold">NASA satellite data</span> to assist farmers in their daily decisions.`}
-
           />
           <FeatureItem icon="/icons/calendar.svg" title="Risk Reduction" />
           <FeatureItem icon="/icons/rain.svg" title="User-friendly access" />
@@ -29,8 +31,15 @@ const Features = () => {
           width={504}
           height={464}
         />
-        <div className="absolute top-[10.3rem] left-[0.3rem]">
-          <Image src="/images/Dirt.png" alt="Dirt" width={100} height={100} objectPosition="cover" className="rounded-full h-28 w-28"/>
+        <div className="absolute top-[12.2rem] left-[2.3rem]">
+          <Image
+            src="/images/Dirt.jpg"
+            alt="Dirt"
+            width={100}
+            height={100}
+            objectPosition="cover"
+            className="rounded-full h-[6.6rem] w-[6.6rem]"
+          />
         </div>
       </div>
       <div className="xl:hidden">
